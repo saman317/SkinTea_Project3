106e605513ffcd9339e805f175f23b85703fb8dc
vmr093 <ranjha.viola@gmail.com>
2024-12-16 18:49:21 -0600

    adding models

--- models/user.js
+++ models/user.js
@@ -12,6 +12,50 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+
+const skinteaSchema = new mongoose.Schema(
+    {
+        productName: {
+            type: String,
+            required: true
+        },
+        productType: {
+            type: String,
+            required: true,
+            enum: ["Skincare", "Makeup" , "Hair"]
+        },
+        reccommendation: {
+            type: String,
+            required: true,
+            enum: ["Holy Grail", "Gotta Have It " , "Ehh", "Regift"]
+
+        },
+        pricePoint: {
+            type: String,
+            required: true,
+            enum: ["$", "$$" , "$$$",]
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        skintea: [skinteaSchema],
+    },
+        {timestamps: true}
+);
+
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         delete returnedObject.hashedPassword;
