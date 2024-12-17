7827dffe35922e1d386c4072380ffc87bd3867bf
vmr093 <ranjha.viola@gmail.com>
2024-12-16 19:07:53 -0600

    adding models

--- models/user.js
+++ models/user.js
@@ -11,6 +11,49 @@ const userSchema = new mongoose.Schema({
         required: true
     },
 });
+const commentSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }
+});
+
+
+const skinteaSchema = new mongoose.Schema(
+    {
+        productName: {
+            type: String,
+            required: true
+        },
+        productType: {
+            type: String,
+            required: true,
+            enum: ["Skincare", "Makeup" , "Hair"]
+        },
+        reccommendation: {
+            type: String,
+            required: true,
+            enum: ["Holy Grail", "Gotta Have It " , "Ehh", "Regift"]
+
+        },
+        pricePoint: {
+            type: String,
+            required: true,
+            enum: ["$", "$$" , "$$$",]
+        },
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        skintea: [skinteaSchema],
+    },
+        {timestamps: true}
+);
 
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
